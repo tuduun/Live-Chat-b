dcde6f0260d5e44f58340517e7a7476d7ab72edc
tuduun <gantulga01@allegheny.edu>
2022-04-22 16:05:24 -0400

    Add class work apr 22

--- docs/scripts/client.js
+++ docs/scripts/client.js
@@ -1,52 +1,81 @@
-var host = "wss://cmpsc-302-chat-server.herokuapp.com"; // TODO: Change to your Heroku host
-
-
-
-
-submit_button1.addEventListener("click",(evt) => {
-    let name = username.value;
-    if(!name) return false;
-    chat.name = name;
-    nameModal.setAttribute("hidden","true");
-    evt.preventDefault();
+var host = "wss://cmpsc-302-chat-server-tuduun.herokuapp.com/"; // : Change to your Heroku host
+submit_button1.addEventListener("click", (evt) => {
+  let name = username.value;
+  if (!name) return false;
+  chat.name = name;
+  nameModal.setAttribute("hidden", "true");
+  evt.preventDefault();
 });
+
+send_message.addEventListener("keydown", (evt) => {
+  if (evt.key == "Enter") {
+    submit_button.click();
+    evt.preventDefault();
+  }
+});
+submit_button1.addEventListener("keydown", (evt) => {
+    if (evt.key == "Enter") {
+      submit_button.click();
+      evt.preventDefault();
+    }
+  });
 
 //: Script WebSocket communication
 var chat = {
-// Create the communication tude
-    socket: new WebSocket(host),
-// Create the function that starts the chat
-    init: () => {
-        chat.socket.addEventListener("message", (evt) => {
-            let msg = evt.data;
-            console.log(msg);
-        });
-// Keep connection alive by sending a message every 1 sec
-        setInterval(() => {
-            chat.send("","ping");
-        }, 1000);
-    },
+  // Create the communication tude
+  socket: new WebSocket(host),
+  // Create the function that starts the chat
+  init: () => {
+    chat.window = document.getElementById("chat-window");
 
-// Add my abiliy to send a message to server
-    send: (message, type) => {
-        let packet = {
-            user: chat.name,
-            text: message,
-            type: type,
-        }
-// 
-        chat.socket.send(
-            JSON.stringify(packet)
-        );
-        return false;
-    }
-}
+    chat.socket.addEventListener("message", (evt) => {
+      let msg = evt.data;
+      console.log(msg);
+      chat.post(msg); //Post to actual div/window
+    });
+    // Keep connection alive by sending a message every 1 sec
+    setInterval(() => {
+      chat.send("", "ping");
+    }, 1000);
+  },
 
+  // Add my abiliy to send a message to server
+  send: (message, type) => {
+    let packet = {
+      user: chat.name,
+      text: send_message.value || message, // || means OR
+      type: type,
+    };
+    if (typeof packet.text !== "string") return false;
+    //
+    chat.socket.send(JSON.stringify(packet));
+    if (type !== "ping") send_message.value = "";
 
+    return false;
+  },
+  //post the chat to the chat.window
 
+  post: (message) => {
+    let msg = document.createElement("p");
+    let text = document.createElement("span");
+    text.className = "chat-msg";
+    text.innerText = `${message}`;
+    msg.appendChild(text);
+    chat.window.appendChild(msg);
+    console.log(msg);
+    chat.scroll();
+    return false;
+  },
+  scroll: () => {
+    let msgs = Array.from(document.getElementsByClassName("chat-msg"));
+    let pos = msgs[msgs.length - 1].offsetTop;
+    chat.window.scrollTo({
+      top: pos,
+      behaviour: "smooth",
+    });
+  },
+};
 
-
-
-//start the chat 
+//start the chat
 
 chat.init();
--- writing/feature.md
+++ writing/feature.md
@@ -2,12 +2,11 @@
 
 ## In no more than 150 words, describe the chat feature you plan to implement.
 
-`TODO`
+I think I will implement a pop-up notification. I am not sure how to implement this, but I'd like to create a notification window and if you mention that person using the @ sign username and everyone, then theat person or user gets a notification.
 
 ## How do you plan to achieve this?
 
-`TODO`
-
+I will consult professor Luman about this. 
 ## Paste the code used to acheive this between the two "fences" below.
 
 You must use at least one of the entries below.
